eb718ad69507277f0e44311ca678ad833299fb53
Claude Gauthier <claude_r_gauthier@hotmail.com>
2016-12-11 21:18:12 -0500

    custom local storage provider and new setup to support this in app.js

--- demos/statemanager/app.js
+++ demos/statemanager/app.js
@@ -1,17 +1,23 @@
-Ext.application({
-    name: 'StateManager',
+Ext.Loader.setPath({
+    "StateManager": "app"
+});
 
-    extend: 'StateManager.Application',
-    
-    autoCreateViewport: 'StateManager.view.main.Main',
-	
-	requires: [
-	   "Ext.state.*"
-	]
-	
-    //-------------------------------------------------------------------------
-    // Most customizations should be made to StateManager.Application. If you need to
-    // customize this file, doing so below this section reduces the likelihood
-    // of merge conflicts when upgrading to new versions of Sencha Cmd.
-    //-------------------------------------------------------------------------
+Ext.onReady(function() {
+    Ext.require(['Ext.state.*', 'StateManager.state.MyLocalStorageProvider'], function() {
+        
+        Ext.state.Manager.setProvider(Ext.create('StateManager.state.MyLocalStorageProvider'));
+        // Ext.state.Manager.setProvider(Ext.create('Ext.state.CookieProvider'));
+        
+        Ext.application({
+            name: 'StateManager',
+        
+            extend: 'StateManager.Application',
+            
+            requires: [
+               'StateManager.view.main.Main'
+            ],
+            
+            autoCreateViewport: 'StateManager.view.main.Main'
+        });        
+    });
 });
